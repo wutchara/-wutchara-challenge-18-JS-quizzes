2bcec5d94b86751140c111fc0ad4dc7a4c4e9d94
wutchara <sstmuham@gmail.com>
2022-08-01 22:40:31 +0700

    Update logic, but not finished yet

--- exercise-21/ans.js
+++ exercise-21/ans.js
@@ -68,8 +68,48 @@ const leapYear = (year) => {
     return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
 }
 
+// TODO:
+const v3 = () => {
+    // 1 Jan 1900 was a Monday.
+    // start => 1 Jan 1901 => Tue
+    // end => 31 Dec 2000
+    const start = {
+        day: 1,
+        month: 0, // Jan
+        year: 1900,
+    }
+
+    const end = {
+        day: 31,
+        month: 11, // Dec
+        year: 2000,
+    }
+
+    const dayInMonthNonLeapYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const dayInMonthLeapYear = [...dayInMonthNonLeapYear];
+    dayInMonthLeapYear[1] = 29; // Fab
+    let count = 0;
+    let tempDate = 1; // Tue 1901/01/01
+
+
+    for (let y = start.year; y <= end.year; y++) {
+        const dayOfMonth = (leapYear(y)) ?  dayInMonthLeapYear : dayInMonthNonLeapYear;
+        for (let m = 0; m < 12; m++) {
+            tempDate += dayOfMonth[m];
+            // sun: 6, mon: 0, tue: 1, wed: 2, tru: 3, fri: 4, sat: 5
+            if (tempDate % 7 === 6) { // sunday
+                tempDate = 6; // reset
+                count++;
+            }
+        }
+    }
+
+    return count;
+};
+
 
 console.log('---- v1:', v1(start, end));
 console.log('---- v2:', v2(start, end));
+console.log('---- v3:', v3());
 
 console.log('============================');
